77117971076ad89db9b2dbda00e2c807961121e9
57ajay <57ajay.u@gmail.com>
2025-09-10 10:15:20 +0530

    nvim setting

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+-- Main init file
+-- Load core configurations
+require('core.options')
+require('core.keymaps')
+require('core.autocmds')
+
+-- Load plugins
+require('plugins')
+
+vim.opt.wrap = true
